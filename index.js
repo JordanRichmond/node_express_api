44b187052b7a4175103cfc584af9a3403eb8d981
Jordan Richmond <richmond42@ymail.com>
2021-09-24 17:42:40 +0100

    connection test

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
 
 import usersRoutes from './src/routes/users.js';
 
+
+// express app
 const app = express();
+
+// connect to MongoDB
+const connectionURI = "mongodb+srv://demo:demo123@pokemontest.l9np2.mongodb.net/node-pokemon?retryWrites=true&w=majority";
+mongoose.connect(connectionURI);
+
+// listen for requests
 const PORT = 5000;
 
 app.use(bodyParser.json());
